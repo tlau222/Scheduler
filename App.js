1dcf47048402dd43217e98e69a66a74a6774726e
Tiffany Lau <58575683+tlau222@users.noreply.github.com>
2021-01-16 19:28:12 -0600

    Fetched data

--- App.js
+++ App.js
@@ -1,45 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import CourseList from './components/CourseList';
 
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-
-};
-
 const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  
+  useEffect(() => {
+    const fetchSchedule =  async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
       <CourseList courses={schedule.courses} />
     </SafeAreaView>
   );
-}
+};
 
 const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title}</Text>
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
 );
 
 const styles = StyleSheet.create({
