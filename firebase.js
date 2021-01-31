ad90ff0f13d0270f39584bc07ff1371002ba53f4
Tiffany Lau <58575683+tlau222@users.noreply.github.com>
2021-01-30 18:08:00 -0600

    Fetched data from the cloud

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+import "firebase/database";
+
+const firebaseConfig = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyA1-47Ew9uOahRnd3NgB8x6KvIH1Hi2-Hw",
+    authDomain: "cs394-scheduler-d975d.firebaseapp.com",
+    databaseURL: "https://cs394-scheduler-d975d-default-rtdb.firebaseio.com",
+    projectId: "cs394-scheduler-d975d",
+    storageBucket: "cs394-scheduler-d975d.appspot.com",
+    messagingSenderId: "893610825875",
+    appId: "1:893610825875:web:d8d4bb8f0345048d1ad566",
+    measurementId: "G-S8J7WBG6CV"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,6 +3,12 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import CourseList from '../components/CourseList';
 import UserContext from '../UserContext';
 import CourseEditScreen from './CourseEditScreen';
+import { firebase } from '../firebase';
+
+const fixCourses = json => ({
+    ...json,
+    courses: Object.values(json.courses)
+});
 
 const ScheduleScreen = ({navigation}) => {
     const user = useContext(UserContext);
@@ -14,17 +20,17 @@ const ScheduleScreen = ({navigation}) => {
         navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
     };
 
-    const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+    // const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
     
     useEffect(() => {
-        const fetchSchedule =  async () => {
-        const response = await fetch(url);
-        if (!response.ok) throw response;
-        const json = await response.json();
-        setSchedule(json);
+        const db = firebase.database().ref();
+        const handleData = snap => {
+          if (snap.val()) setSchedule(fixCourses(snap.val()));
         }
-        fetchSchedule();
-    }, []);
+        db.on('value', handleData, error => alert(error));
+        return () => { db.off('value', handleData); };
+      }, []);
+
     return (
         <SafeAreaView style={styles.container}>
             <Banner title={schedule.title} error={schedule.error} />
